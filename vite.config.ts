a718a5240fdc044868f027e9cbd1c7b4952d9155
Tim <14timothydeleon@gmail.com>
2025-05-08 21:02:48 -0400

    configuring githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
