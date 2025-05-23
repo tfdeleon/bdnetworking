ce56bf677a3e7eb3a57fdb2026ab13cbfbea0c23
tim <tfdeleon@buffalo.edu>
2025-05-23 01:17:44 -0400

    Trigger rebuild for available-times.js

--- src/server/api/available-times.js
+++ src/server/api/available-times.js
@@ -1,17 +1,17 @@
 import { getAvailableTimeSlots } from "../calendar.js";
 
 export default async function handler(req, res) {
-  // Set CORS headers
+
   res.setHeader("Access-Control-Allow-Origin", "https://www.bdlvsolutions.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle preflight request
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
-  // Only allow GET method
+
   if (req.method !== "GET") {
     res.setHeader("Allow", "GET");
     return res.status(405).json({ error: "Method Not Allowed" });
